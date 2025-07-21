6f63cd995571f7c78ac1657bd9f11848bf87b894
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-07-21 18:20:47 +0200

    tested inductive with arrow parameter

--- Test/Tests.fs
+++ Test/Tests.fs
@@ -58,3 +58,25 @@ Inductive color: Type :=
     Inductive(newType = TypeExpr.Simple "color", baseType = TypeExpr.Simple "Type", cases = colors)
 
   Assert.Equal<AST>(expected, actual)
+
+[<Fact>]
+let ``inductive with function`` () =
+  let text =
+    "
+  Inductive bla : Type :=
+  | gogo (f: nat -> nat).
+"
+
+  let actual = parseInductive text
+
+  let expected =
+    Inductive(
+      newType = TypeExpr.Simple "bla",
+      baseType = TypeExpr.Simple "Type",
+      cases =
+        [ simpleParams
+            "gogo"
+            [ TypeParams.TypeParams([ "f" ], TypeExpr.Func(TypeExpr.Simple "nat", TypeExpr.Simple "nat")) ] ]
+    )
+
+  Assert.Equal<AST>(expected, actual)
